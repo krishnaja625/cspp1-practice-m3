e85162dc38e33b5e2d483879c59c9952335a621c
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-01 16:30:54 +0530

    reverse_even_iterate

---
+++ compare_AB.py
@@ -0,0 +1,4 @@
+"""String Comparison"""
+V1=input('enter var1')
+V2=input('enter var2')
+print(type(V1))
---
+++ iterate_even_reverse.py
@@ -0,0 +1,7 @@
+"""Reverse iterate"""
+print('Hello!')
+I = 10
+while I > 0:
+    print(I)
+    I = I-2
+    
